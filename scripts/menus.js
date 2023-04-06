1b2acd069930b5cc149120e608e2519c582b07e2
TacyPW <48765699+TacyPW@users.noreply.github.com>
2023-04-06 12:34:30 -0700

    fixed close_info, removed pers

--- scripts/menus.js
+++ scripts/menus.js
@@ -2,40 +2,25 @@ function open_menu() {
     close_artist();
     document.getElementById("menu-open").style.display = "block";
     document.getElementById("menu-closed").style.display = "none";
-    hide_pers();
+
 }
 
 function close_menu() {
     document.getElementById("menu-closed").style.display = "block";
     document.getElementById("menu-open").style.display = "none";
-    show_pers();
+
 }
 
 function open_info() {
     close_menu();
     document.getElementById("info-open").style.display = "block";
     document.getElementById("info-closed").style.display = "none";
-    hide_pers();
+
 }
 
-function close_artist() {
+function close_info() {
     document.getElementById("info-closed").style.display = "block";
     document.getElementById("info-open").style.display = "none";
-    show_pers();
+
 }
 
-function hide_pers() {
-    if (document.getElementById("perspectives")) {
-        document.getElementById("perspectives").style.display = "none";
-    }
-    // document.getElementsByClassName("layer0").style.zIndex = "-1";
-
-    $(".layer0").css({'z-index' : -1});
-}
-
-function show_pers() {
-    if (document.getElementById("perspectives")) {
-        document.getElementById("perspectives").style.display = "block";
-    }
-    $(".layer0").css({'z-index' : 2});
-}
