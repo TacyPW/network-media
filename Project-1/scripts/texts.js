21ff3118fdbfa105107e3a384ffadd87f03972f3
TacyPW <48765699+TacyPW@users.noreply.github.com>
2023-04-13 23:16:11 -0700

    texts split up into stanza > line arrays

--- Project-1/scripts/texts.js
+++ Project-1/scripts/texts.js
@@ -1,14 +1,38 @@
-let es_text;
-let en_text;
+var es_text;
+var en_text;
+
+let en_stanzas = [];
+let es_stanzas = [];
+
+let en_poem = [];
+let es_poem = [];
+
+
 
 en_text = $.get('texts/masters.txt', function(data) {
+    en_text = data;
+    console.log("english loaded");
+    breakup(en_text, en_stanzas, en_poem);
+    console.log(en_poem);
     en_text = data.replace(/\n/g, " <br>\n");
     $("#entext").append(en_text);
- }, 'text');
+}, 'text');
+
 
 
 es_text = $.get('texts/maestros.txt', function(data) {
+    es_text = data;
+    console.log("español cargado");
+    breakup(es_text, es_stanzas, es_poem);
+    console.log(es_poem);
     es_text = data.replace(/\n/g, " <br>\n");
-    $("#estext").append(es_text);
- }, 'text');
+    $("#estext").append(en_text);
+}, 'text');
+
+function breakup(text, stanzas, poem) {
+    stanzas = text.split("\n\n");
+    for (let i = 0; i < stanzas.length; i++) {
+        poem.push(stanzas[i].split("\n"));
+    }
+}
 
