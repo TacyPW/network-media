860d8b2b103f036a5af6b1079c2ee8f16a3cd755
TacyPW <48765699+TacyPW@users.noreply.github.com>
2023-04-24 12:23:49 -0700

    added line 3, style data for image objects

--- Project-1/scripts/figures.js
+++ Project-1/scripts/figures.js
@@ -10,6 +10,7 @@ function showfigure(id) {
             let currimg = figdata.imgpath;
             let curralt = figdata.alttext; 
             let currimgel = $("<img class=\"figure grid-50 mobile-grid-100\" src=\"" + currimg + "\" alt=\"" + curralt + "\"></img>").text(this.en);
+            $(currimgel).css(figdata.style);
             $(currimgel).css("top", String(currel.offsetTop) + "px");
             $("#" + id).after(currimgel);
         }
@@ -22,6 +23,14 @@ Figures = {
         rendered: false,
         imgpath: "assets/images/gun.png",
         alttext: "ak-47 machine gun",
-        textpath: "assets/descriptions/line2.html" 
+        textpath: "assets/descriptions/line2.html",
+        style: {"width":"40%", "left":"5vw" }
+    },
+    line3: {
+        rendered: false,
+        imgpath: "assets/images/warplane.png",
+        alttext: "Lockheed Martin F-22 Fighter Jet",
+        textpath: "assets/descriptions/line3.html",
+        style: {"width":"30%", "left":"60vw", "transform":"translate(0,-15px)" } 
     }
 }
