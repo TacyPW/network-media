c3baccb80e9adc51440f11170489b06edf7ce9b0
TacyPW <48765699+TacyPW@users.noreply.github.com>
2023-04-24 10:45:53 -0700

    basic struct

--- Project-1/scripts/figures.js
+++ Project-1/scripts/figures.js
@@ -1,5 +1,8 @@
-
-
+function showfigure(id) {
+    // search figures for id and then if so, make image element $after
+    // (in css set image elements to z index below)
+    // add description overlay
+  }
 
 
 Figures = {
