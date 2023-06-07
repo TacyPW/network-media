2e48feca529554760e1ccd3594cb56438d3b9b30
TacyPW <48765699+TacyPW@users.noreply.github.com>
2023-06-06 20:47:24 -0700

    script works now had to tell it the func was async

--- Project-3/test-site/script.js
+++ Project-3/test-site/script.js
@@ -1,1 +1,16 @@
-// Add JavaScript functionality here if needed
+$(function() {
+    $("#header").load("header.html", function() {
+      var $navbar = $('.navbar');
+      var $menu = $('.menu');
+  
+      // Toggle menu open/close when hamburger is clicked
+      $navbar.on('click', '.hamburger', function() {
+        $menu.toggleClass('open');
+      });
+  
+      // Close menu when a menu item is clicked
+      $menu.on('click', 'a', function() {
+        $menu.removeClass('open');
+      });
+    });
+  });
